6e8d61bbc26f7dd01c93e44c56a81e728b4a3821
dmerejkowsky <dmerejkowsky@aldebaran-robotics.com>
2012-01-03 17:21:11 +0100

    src/lasermain.cpp: remove dead code
    (win32 specific, or to use allaser in remote)

--- src/lasermain.cpp
+++ src/lasermain.cpp
@@ -12,27 +12,16 @@
 #include <alcommon/albroker.h>
 #include <alcommon/almodule.h>
 #include <alcommon/albrokermanager.h>
-#include <alcommon/altoolsmain.h>
 
 using namespace std;
 using namespace AL;
 
 #include "allaser.h"
 
-#ifndef LASER_IS_REMOTE
-
-#ifdef _WIN32
-#define ALCALL __declspec(dllexport)
-#else
-#define ALCALL
-#endif
-#else
-#define ALCALL
-#endif
-
 extern "C"
 {
-ALCALL int _createModule( boost::shared_ptr<ALBroker> pBroker )
+
+int _createModule( boost::shared_ptr<ALBroker> pBroker )
 {
   // init broker with the main broker instance
   // from the parent executable
@@ -42,20 +31,4 @@ ALCALL int _createModule( boost::shared_ptr<ALBroker> pBroker )
   return 0;
 }
 
-ALCALL int _closeModule(  )
-{
-  return 0;
-}
-}
-
-
-#ifdef LASER_IS_REMOTE
-int main(int argc, char *argv[] )
-{
-  // pointer on createModule
-  TMainType sig;
-  sig = &_createModule;
-  // call main
-  ALTools::mainFunction("laser",argc, argv,sig);
-}
-#endif
+} // extern "C"
